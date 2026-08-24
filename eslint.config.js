1a38bf614ce56f1f24c4a81001a59f18fb56c2fc
Alexander Soell <Alexander.Soell@csi-online.de>
2026-08-24 12:42:07 +0200

    fix: cleaned up linting setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,23 @@ import oxlint from "eslint-plugin-oxlint";
 import eslint from "@eslint/js";
 
 export default [
+  {
+    ignores: [
+      ".build/**",
+      ".dist/**",
+      ".schema-peer/**",
+      "build/**",
+      "coverage/**",
+    ],
+  },
+  {
+    languageOptions: {
+      globals: {
+        console: "readonly",
+        process: "readonly",
+      },
+    },
+  },
   eslint.configs.recommended,
   ...oxlint.configs["flat/all"],
 ];
